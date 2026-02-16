758d6d49b783520c1a0d047b9f5e9f64fd3c748d
twingz226 <twingzmarfil@.com>
2026-02-16 22:43:18 +0800

    emailverified

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -22,6 +22,7 @@ class DatabaseSeeder extends Seeder
             'name' => 'Admin User',
             'email' => 'admin@example.com',
             'password' => Hash::make('password'),
+            'email_verified_at' => now(),
         ]);
     }
 }
